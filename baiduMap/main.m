a8dadba3be89509fcf5e528a225588c42526c15a
caijianwei <caijianwei58@yahoo.cn>
2013-07-31 11:21:07 +0800

    baiduMap
    baiduMap

---
+++ baiduMap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  baiduMap
+//
+//  Created by 田地 on 13-7-24.
+//  Copyright (c) 2013年 com.isoftstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BaiDuAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BaiDuAppDelegate class]));
+    }
+}
