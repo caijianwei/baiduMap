6436f94901c90ae6ab8c296c532962c30db75c69
caijianwei <caijianwei58@yahoo.cn>
2013-11-15 10:06:37 +0800

    hell
    你

---
+++ baiduMap/BaiDuAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BaiDuAppDelegate.h
+//  baiduMap
+//
+//  Created by 田地 on 13-7-24.
+//  Copyright (c) 2013年 com.isoftstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaiDuAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
